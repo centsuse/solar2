3f2a5a29607dae30129e4222f5efe6486e3b3186
centsuse <centsuse@gamil.com>
2020-06-20 17:26:21 +0800

    init

---
+++ src/main/java/com/centsuse/solar2/Solar2Application.java
@@ -0,0 +1,13 @@
+package com.centsuse.solar2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Solar2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Solar2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/centsuse/solar2/Solar2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centsuse.solar2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Solar2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
